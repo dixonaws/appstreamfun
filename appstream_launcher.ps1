8fb6ca4d63b4e13c13042e019b8932be83605484
dixonaws@amazon.com <dixonaws@amazon.com>
2019-07-26 09:45:35 +0200

    added appstreaminvoker_auth

--- appstream_launcher.ps1
+++ appstream_launcher.ps1
@@ -1,8 +1,10 @@
-﻿$request_uri="https://99xoxbk3x1.execute-api.eu-central-1.amazonaws.com/api/"
+﻿# This is the URI of the API Gateway endpoint
+$request_uri="https://99xoxbk3x1.execute-api.eu-central-1.amazonaws.com/api/"
 
 $json_response = Invoke-WebRequest -UseBasicParsing -URI $request_uri`
 $response=ConvertFrom-Json $json_response
 
 $streaming_url=$response.StreamingURL
 
+# Start a browser session with the streaming url
 Start-Process -FilePath "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" -ArgumentList "$streaming_url", "--new-window", "--app"
